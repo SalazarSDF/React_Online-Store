6320894ab35765f6fa4b4b7e16cd5484e4af8f72
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2023-03-23 16:11:32 +0300

    add: basic routes for now, will fix later

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @jsxImportSource @emotion/react */
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { SortOptions } from "./components/SortOptions";
+import { ProductsList } from "./components/ProductsList";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function AppRoutes() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Routes>
+      <Route path="/shopcart" element={<h1>SHOPACART</h1>}></Route>
+    </Routes>
+  );
+}
+// function AppRoutes() {
+//   return (
+//     <Routes>
+//       <Route path="/list" element={<ReadingListScreen />} />
+//       <Route path="/finished" element={<FinishedScreen />} />
+//       <Route path="/discover" element={<DiscoverBooksScreen />} />
+//       <Route path="/book/:bookId" element={<BookScreen />} />
+//       <Route path="*" element={<NotFoundScreen />} />
+//     </Routes>
+//   )
+// }
+function Logo() {
+  return (
+    <Link
+      css={{
+        color: "white",
+        textDecoration: "none",
+      }}
+      to="/"
+    >
+      Online-Shop
+    </Link>
+  );
 }
 
-export default App
+function TrashBag() {
+  return (
+    <Link
+      css={{
+        color: "white",
+        textDecoration: "none",
+      }}
+      to="/shopcart"
+    >
+      <FaShoppingCart />
+      Cart
+    </Link>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <nav
+        css={{
+          position: "sticky",
+          backgroundColor: "blue",
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "0 auto",
+          width: "100%",
+          height: "80px",
+          alignItems: "center",
+        }}
+      >
+        <Logo />
+        <TrashBag />
+      </nav>
+      <main css={{ display: "grid", gridTemplateColumns: "1fr 3fr" }}>
+        <SortOptions />
+        <ProductsList />
+      </main>
+      <AppRoutes />
+    </>
+  );
+}
+
+
+
+export default App;
